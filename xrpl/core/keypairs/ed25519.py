9cd78d24e48ce60c50387c6e252a65d8c71a95e4
Shi Cheng <cshi@ripple.com>
2026-02-03 00:24:45 +0800

    release stable 4.6.16

--- xrpl/core/keypairs/ed25519.py
+++ xrpl/core/keypairs/ed25519.py
@@ -28,7 +28,7 @@ class ED25519(CryptoImplementation):
     ) -> Tuple[str, str]:
         """
         Derives a key pair in Ed25519 format for use with the XRP Ledger from a
-        seed value. big change
+        seed value. beta change b2
 
         Args:
             decoded_seed: The Ed25519 seed to derive a key pair from, as bytes.
